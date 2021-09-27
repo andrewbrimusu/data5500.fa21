e1198064db0fe338cb13077f97cbc4d7118f65cd
Ubuntu <ubuntu@ip-172-31-11-182.us-west-1.compute.internal>
2021-09-27 16:50:09 +0000

    updated code

---
+++ week5_examples/recursion.py
@@ -0,0 +1,14 @@
+lst = ["diet coke", "rugby", "hiking", "pizza", "fajitas"]
+
+for item in lst:
+    print(item)
+    
+    
+def print_items(lst):
+    # base case
+    
+    # logic / recursive
+    
+    # return
+    
+    

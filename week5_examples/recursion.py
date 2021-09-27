8b548c09025b2c08a6741b2f2519c4d5b3e6316a
Ubuntu <ubuntu@ip-172-31-11-182.us-west-1.compute.internal>
2021-09-27 17:58:11 +0000

    updated code

--- week5_examples/recursion.py
+++ week5_examples/recursion.py
@@ -1,14 +1,26 @@
-lst = ["diet coke", "rugby", "hiking", "pizza", "fajitas"]
+lst = ["diet coke", "rugby", "hiking", "pizza", "fajitas", "football", "basketball", "animals", "people"]
 
 for item in lst:
     print(item)
     
     
-def print_items(lst):
+print("--------")
+
+
+def print_items(lst, idx):
     # base case
+    if idx >= len(lst):
+        return
     
-    # logic / recursive
+    # print item / recursive
+    print(idx, lst[idx])
+    if idx == 0:
+        idx = 2
+    else:
+        idx *= 2
+        
+    print_items(lst, idx)
     
     # return
     
-    
+print_items(lst, 0)

cb36103687c41f6ddb691f65fb602ff17b1add28
Ubuntu <ubuntu@ip-172-31-11-182.us-west-1.compute.internal>
2021-11-03 20:27:41 +0000

    updated code

--- week10_regression/regression_byhand.py
+++ week10_regression/regression_byhand.py
@@ -1,6 +1,14 @@
+import os
+# os.system("sudo pip3 install networkx")
+# os.system("sudo apt-get install libjpeg-dev zlib1g-dev")
+# os.system("sudo pip3 install Pillow")
+# os.system("sudo pip3 install matplotlib")
+
+
 import numpy as np
 import matplotlib.pyplot as plt
  
+
 def estimate_coef(x, y):
     # number of observations/points
     n = np.size(x)
